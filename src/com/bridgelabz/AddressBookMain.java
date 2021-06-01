9e1aee24d62a62b72de8a48c3f074875b9a8d1e4
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-01 12:24:54 +0530

    Added Address Book

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+class AddressBookMain {
+	    public static void main(String[]args) {
+	        System.out.println("Wellcome to Address Book Program!");
+
+	        AddressBook contact = new AddressBook("Prathana", "chudhari", "xyz", "Munbai", "Maharashtra", "751011", "7978715564", "prarthanachaudhari@gmail.com");
+	        contact.printContactDetails();
+	    }
+	}
+	
+
+
