725ba89fc53e2151d37a1819b7408a0367d88f45
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-21 21:27:37 +0530

    Added data

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,132 +1,201 @@
-package com.bridgelabz;
-import java.util.*;
+/**
+ * Here i have imported Scanner to take user input.
+ * imported ArrayList to store the values in an ArrayList.
+ */
 public class AddressBook {
+	public String firstname, lastname;
+	public String address, city, state;
+	public int zip;
+	public long phoneNumber;
+	public String email;
 
-	
-	    public String firstname;
-		public String lastname;
-	    public String address, city, state;
-	    public long zip;
-	    public long phoneNumber;
-	    public String email;
+	/**
+	 * This is a parameterized constructor having the given parameters.
+	 * @param firstname
+	 * @param lastname
+	 * @param address
+	 * @param city
+	 * @param state
+	 * @param zip
+	 * @param phonenumber
+	 * @param email
+	 */
+	public AddressBook(String firstname, String lastname, String address, String city, String state, int zip, long phonenumber, String email) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phonenumber;
+		this.email = email;
+	}
 
-	    public AddressBook(String firstname, String lastname, String address, String city, String state, long zip, long phonenumber, String email) {
-	        this.firstname = firstname;
-	        this.lastname = lastname;
-	        this.address = address;
-	        this.city = city;
-	        this.state = state;
-	        this.zip = zip;
-	        this.phoneNumber = phonenumber;
-	        this.email = email;
-	    }
+	/**
+	 * these get methods are used to get the values according to the user input.
+	 * @return
+	 */
+	public String getFirstName() {
+		return firstname;
+	}
+	public void setFirstName(String firstName) {
+		this.firstname = firstName;
+	}
+	public String getLastName() {
+		return lastname;
+	}
+	public void setLastName(String lastName) {
+		this.lastname = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public long getPhoneNo() {
+		return phoneNumber;
+	}
+	public void setPhoneNo(long phno) {
+		this.phoneNumber = phno;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-	    /**
-	     * This printContactDetails method will print all the values given below.
-	     */
-	    public void printContactDetails() {
-	        System.out.println("Firstname : " + firstname);
-	        System.out.println("Lastname : " + lastname);
-	        System.out.println("Address : " + address);
-	        System.out.println("City : " + city);
-	        System.out.println("State : " + state);
-	        System.out.println("Zip : " + zip);
-	        System.out.println("PhoneNumber : " + phoneNumber);
-	        System.out.println("Email : " + email);
-	    }
-	    /**
-	     * these get methods are used to get the values according to the user input.
-	     * @return
-	     */
-	        public String getFirstName() {
-	            return firstname;
-	        }
-	        public void setFirstName(String firstName) {
-	            this.firstname = firstName;
-	        }
-	        public String getLastName() {
-	            return lastname;
-	        }
-	        public void setLastName(String lastName) {
-	            this.lastname = lastName;
-	        }
-	        public String getAddress() {
-	            return address;
-	        }
-	        public void setAddress(String address) {
-	            this.address = address;
-	        }
-	        public String getCity() {
-	            return city;
-	        }
-	        public void setCity(String city) {
-	            this.city = city;
-	        }
-	        public String getState() {
-	            return state;
-	        }
-	        public void setState(String state) {
-	            this.state = state;
-	        }
-	        public long getZip() {
-	            return zip;
-	        }
-	        public void setZip(int zip) {
-	            this.zip = zip;
-	        }
-	        public long getPhoneNo() {
-	            return phoneNumber;
-	        }
-	        public void setPhoneNo(long phno) {
-	            this.phoneNumber = phno;
-	        }
-	        public String getEmail() {
-	            return email;
-	        }
-	        public void setEmail(String email) {
-	            this.email = email;
-	        }
-	        /**
-	         * This method will display the data of the addressbook.
-	         * nameToEdit1 
-	         */
-	        public void editContact() {
-	            Scanner sc = new Scanner(System.in);
-	            System.out.println("Edit Firstname : ");
-	            String firstname = sc.nextLine();
-	            setFirstName(firstname);
+	/**
+	 * This method will display the data of the addressbook.
+	 */
+	public void printContactDetails() {
+		System.out.println("Firstname : " + getFirstName());
+		System.out.println("Lastname : " + getLastName());
+		System.out.println("Address : " + getAddress());
+		System.out.println("City : " + getCity());
+		System.out.println("State : " + getState());
+		System.out.println("Zip : " + getZip());
+		System.out.println("PhoneNumber : " + getPhoneNo());
+		System.out.println("Email : " + getEmail());
 
-	            System.out.println("Edit Lastname : ");
-	            String lastname = sc.nextLine();
-	            setLastName(lastname);
+	}
+}/**
+ * Here i have imported Scanner to take user input.
+ * imported ArrayList to store the values in an ArrayList.
+ */
+public class AddressBook {
+	public String firstname, lastname;
+	public String address, city, state;
+	public int zip;
+	public long phoneNumber;
+	public String email;
 
-	            System.out.println("Edit Address : ");
-	            String address = sc.nextLine();
-	            setAddress(address);
+	/**
+	 * This is a parameterized constructor having the given parameters.
+	 * @param firstname
+	 * @param lastname
+	 * @param address
+	 * @param city
+	 * @param state
+	 * @param zip
+	 * @param phonenumber
+	 * @param email
+	 */
+	public AddressBook(String firstname, String lastname, String address, String city, String state, int zip, long phonenumber, String email) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phonenumber;
+		this.email = email;
+	}
 
-	            System.out.println("Edit City : ");
-	            String city = sc.nextLine();
-	            setCity(city);
+	/**
+	 * these get methods are used to get the values according to the user input.
+	 * @return
+	 */
+	public String getFirstName() {
+		return firstname;
+	}
+	public void setFirstName(String firstName) {
+		this.firstname = firstName;
+	}
+	public String getLastName() {
+		return lastname;
+	}
+	public void setLastName(String lastName) {
+		this.lastname = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public long getPhoneNo() {
+		return phoneNumber;
+	}
+	public void setPhoneNo(long phno) {
+		this.phoneNumber = phno;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-	            System.out.println("Edit State : ");
-	            String state = sc.nextLine();
-	            setState(state);
+	/**
+	 * This method will display the data of the addressbook.
+	 */
+	public void printContactDetails() {
+		System.out.println("Firstname : " + getFirstName());
+		System.out.println("Lastname : " + getLastName());
+		System.out.println("Address : " + getAddress());
+		System.out.println("City : " + getCity());
+		System.out.println("State : " + getState());
+		System.out.println("Zip : " + getZip());
+		System.out.println("PhoneNumber : " + getPhoneNo());
+		System.out.println("Email : " + getEmail());
 
-	            System.out.println("Edit Zip : ");
-	            int zip = sc.nextInt();
-	            setZip(zip);
-
-	            System.out.println("Edit PhoneNumber : ");
-	            long phonenumber = sc.nextLong();
-	            sc.nextLine();
-	            setPhoneNo(phonenumber);
-
-	            System.out.println("Edit Email : ");
-	            String email = sc.nextLine();
-	            setEmail(email);
-	        }
-
-			
-	        }
-	    
-
+	}
+}
