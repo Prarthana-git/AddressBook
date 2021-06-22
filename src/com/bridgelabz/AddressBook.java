a7871860c6caf0287ed36b0713fe3d2778153fcd
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-22 18:58:42 +0530

    View Person by city or by state

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -32,70 +32,24 @@ public class AddressBook {
 	}
 
 	/**
-	 * these get methods are used to get the values according to the user input.
-	 * @return
+	 * this constructor will work if we'll not pass any parmeters.
 	 */
-	public String getFirstName() {
-		return firstname;
-	}
-	public void setFirstName(String firstName) {
-		this.firstname = firstName;
-	}
-	public String getLastName() {
-		return lastname;
-	}
-	public void setLastName(String lastName) {
-		this.lastname = lastName;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public int getZip() {
-		return zip;
-	}
-	public void setZip(int zip) {
-		this.zip = zip;
-	}
-	public long getPhoneNo() {
-		return phoneNumber;
-	}
-	public void setPhoneNo(long phno) {
-		this.phoneNumber = phno;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	/**
-	 * This method will display the data of the addressbook.
-	 */
-	public void printContactDetails() {
-		System.out.println("Firstname : " + getFirstName());
-		System.out.println("Lastname : " + getLastName());
-		System.out.println("Address : " + getAddress());
-		System.out.println("City : " + getCity());
-		System.out.println("State : " + getState());
-		System.out.println("Zip : " + getZip());
-		System.out.println("PhoneNumber : " + getPhoneNo());
-		System.out.println("Email : " + getEmail());
+	public AddressBook
+	{
 
 	}
+
+	public toString() {
+		return "AddressBook{" +
+				"firstname='" + firstname + '\'' +
+				", lastname='" + lastname + '\'' +
+				", address='" + address + '\'' +
+				", city='" + city + '\'' +
+				", state='" + state + '\'' +
+				", zip=" + zip +
+				", phoneNumber=" + phoneNumber +
+				", email='" + email + '\'' +
+				'}';
+	}
+}
 }
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,67 +1,16 @@
-import java.util.Scanner;
-/**
- * Added AddressBookmain class to create object of Main class.
- */
-public class AddressbookMain {
-	private void choice() {
-		boolean result = true;
-		Scanner input = new Scanner(System.in);
-		AddressBookArray addressBook = new AddressBookArray();
+import com.bridgelabz
 
-		/**
-		 * switch case for each cases to be performed
-		 * According to the options it'll call the method.
-		 */
-		while(result) {
-			System.out.println("Enter option to make changes : \n[*press 1 to add new Contact."
-					+"\n*press 2 to Edit existing contacts."
-					+"\n*press 3 to delete a contact detail."
-					+"\n*press 4 to search by city."
-					+"\n*press 5 to search by State."
-					+ "\n*press 0 to quit ]");
-			int option = input.nextInt();
-			switch (option) {
-				case 1 :
-					addressBook.addContact();
-					break;
-				case 2 :
-					System.out.println("Enter your firstname to Edit your details..");
-					String firstname = input.next();
-					addressBook.editContact(firstname);
-					break;
-				case 3 :
-					System.out.println("Enter your Firstname to delete your details :");
-					String firstName = input.next();
-					addressBook.deleteContact(firstName);
-					break;
-				case 4:
-					System.out.println("Enter your City name to search Person details: ");
-					String city=input.next();
-					addressBook.searchPersonByCity(city);
-					break;
-				case 5:
-					System.out.println("Enter your State name to search person details: ");
-					String state=input.next();
-					addressBook.searchPersonByState(state);
-					break;
-				default:
-					result = false;
-			}
+public class AddressbookMain{
+	public static void main (String[] args)
+	{
+		AddressbookImplement bookBuilder=new AddressbookImplement();
+		bookBuilder.makechoice();
+		boolean condition=true;
+		while(condition)
+		{
+			boolean condition=bookBuilder.makechoice();
+			if (condition == false)
+				break;
 		}
 	}
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Address Book");
-		/**
-		 * Here i have created an object of Address book class with the parameters
-		 * it will assign inside the constructor and initialised too.
-		 */
-		AddressBook addressBook = new AddressBook("Prarthana", "Chaudhari", "Mumbai",
-				"Mumbai", "Maharashtra", 256321,452133654,"prarthanachaudhari@gmail.com");
-		addressBook.printContactDetails();
-		/**
-		 * Here i have created one object of Addressbookmain class named main.
-		 * which will call the choice method to execute all the functions.
-		 */
-		AddressbookMain main = new AddressbookMain();
-		main.choice();
-	}
+}
